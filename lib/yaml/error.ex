c82ff34f1617ec35c6aec2f62bc386db494b0fdd
Sheharyar Naseer <hello@sheharyar.me>
2018-11-05 20:58:31 -0500

    Clean up ParsingError module

--- lib/yaml/error.ex
+++ lib/yaml/error.ex
@@ -1,9 +1,42 @@
 defmodule YAML.ParsingError do
-  defexception [:message, :location, :code]
+  defexception [:message, :location, :type]
 
-  def format([error | _rest]), do: format(error)
-  def format({_type, _level, message, loc1, loc2, _code, _token, _other}) do
-    {:invalid_yaml, [message: message, location: {loc1, loc2}]}
+
+  # Exception Callbacks
+  # -------------------
+
+
+  def message(error) do
+    "#{error.message} (at #{inspect(error.location)})"
   end
+
+
+
+  # Helper API
+  # ----------
+
+
+  def build_tuple(error) do
+    {:error, {:invalid_yaml, format(error)}}
+  end
+
+
+  def build_error(error) do
+    struct(YAML.ParsingError, format(error))
+  end
+
+
+
+  # Private Methods
+  # ---------------
+
+
+  defp format([error | _rest]), do: format(error)
+  defp format({type, _level, message, loc1, loc2, _code, _token, _other}) do
+    [type: normalize(type), message: to_string(message), location: {loc1, loc2}]
+  end
+
+  defp normalize(:yamerl_parsing_error), do: :parse_error
+  defp normalize(type), do: type
 
 end
