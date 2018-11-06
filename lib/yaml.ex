fd087cc1bd424a4a02e97385f83dd86c37d793aa
Sheharyar Naseer <hello@sheharyar.me>
2018-11-05 20:58:50 -0500

    Re-raising exception in decode!

--- lib/yaml.ex
+++ lib/yaml.ex
@@ -6,11 +6,15 @@ defmodule YAML do
   def decode(binary) do
     {:ok, YAML.Parser.parse!(binary)}
   catch
-    {:yamerl_exception, error} ->
-      {:error, YAML.ParsingError.format(error)}
+    {:yamerl_exception, error} -> YAML.ParsingError.build_tuple(error)
   end
 
   def decode!(binary) do
     YAML.Parser.parse!(binary)
+  catch
+    {:yamerl_exception, error} ->
+      error = YAML.ParsingError.build_error(error)
+      reraise(error, __STACKTRACE__)
   end
+
 end
