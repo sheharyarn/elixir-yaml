3d59016c15981641df8d0d3248f21e606e77620a
Sheharyar Naseer <hello@sheharyar.me>
2018-11-06 21:09:32 -0500

    Clean up Mixfile

--- mix.exs
+++ mix.exs
@@ -1,25 +1,81 @@
 defmodule YAML.MixProject do
   use Mix.Project
 
+  @app     :yaml
+  @name    "YAML"
+  @version "0.1.0"
+  @github  "https://github.com/sheharyarn/#{@app}"
+  @author  "Sheharyar Naseer"
+  @license "MIT"
+
+
+  # NOTE:
+  # To publish package or update docs, use the `docs`
+  # mix environment to not include support modules
+  # that are normally included in the `dev` environment
+  #
+  #   MIX_ENV=docs hex.publish
+  #
+
+
   def project do
     [
-      app: :yaml,
-      version: "0.1.0",
-      elixir: "~> 1.7",
-      start_permanent: Mix.env() == :prod,
-      deps: deps()
+      # Project
+      app:           @app,
+      version:       @version,
+      elixir:        "~> 1.6",
+      description:   description(),
+      package:       package(),
+      deps:          deps(),
+      elixirc_paths: elixirc_paths(Mix.env),
+
+      # ExDoc
+      name:         @name,
+      source_url:   @github,
+      homepage_url: @github,
+      docs: [
+        main:       @name,
+        canonical:  "https://hexdocs.pm/#{@app}",
+        extras:     ["README.md"]
+      ]
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
+
+  # BEAM Application
   def application do
     [extra_applications: [:logger]]
   end
 
-  # Run "mix help deps" to learn about dependencies.
+
+  # Dependencies
   defp deps do
     [
-      {:yamerl, "~> 0.7.0"},
+      {:yamerl,  "~> 0.7.0"},
+      {:ex_doc,  ">= 0.0.0", only: :docs},
+      {:inch_ex, ">= 0.0.0", only: :docs},
+    ]
+  end
+
+
+  # Compilation Paths
+  defp elixirc_paths(_env),  do: ["lib"]
+
+
+  # Package Description
+  defp description do
+    "Simple & Powerful Elixir wrapper for the Mnesia Database"
+  end
+
+
+  # Package Information
+  defp package do
+    [
+      name: @app,
+      maintainers: [@author],
+      licenses: [@license],
+      files: ~w(mix.exs lib README.md),
+      links: %{"Github" => @github}
     ]
   end
 end
